1ea977954a69d896ca904d5593da1819f75d5600
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-09-27 17:02:06 +0530

    init frontend

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className=''>
+         <div className='w-screen h-[100vh] flex justify-center items-center'>
+             <h1 className='text-9xl font-bold tracking-tight font-sans'>
+                Table Booking System
+             </h1>
+         </div>
+    </div>
+  )
+}
+
+export default App
